b94e49be89264f789356f0205f0d77794a67c335
abbaharuna <abbaharuna56@gmail.com>
2023-05-19 19:23:47 +0100

    Added functionality for printing an intro message and allowing the user to choose what they play as.

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,12 +1,47 @@
-#include <iostream>
+#include "TicTacToe.h"
 
 using namespace std;
 
+
+bool checkGame(char gameSquaresIn[rows][colums]){
+
+}
+
+/*char[][] computerPlayer(){
+
+}*/
+
 int main (){
     bool gameOver = false;
-    char gameSquares[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    char gameSquares[rows][colums] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    char userChoice;
+    char computerChoice;
+    Player currentPlayer;
 
-    cout<<"Working";
+    //Start message for user
+    cout<<"Welcome to TicTacToe. To choose noughts enter o, to choose crosses enter x.";
+    cin>>userChoice;
+
+    bool userChoiceSet = false;
+
+    while(!userChoiceSet){
+        if (userChoice == 'o'){
+            computerChoice = 'x';
+            currentPlayer = Player::computer;
+        }
+        else if(userChoice == 'x'){
+            computerChoice = 'o';
+            currentPlayer = Player::user;
+        }
+        else{
+            cout<<"You have entered an invalid input. To choose noughts enter o, to choose crosses enter x.";
+            cin>>userChoice;
+        }
+    }
+
+    while (!gameOver){
+        gameOver = checkGame(gameSquares);
+    }
 
     return 0;
 }
